9bbbc22f50adc6ff57ef3f49d2b5fc5e085743d5
lqur4 <2233073359@correo.ler.uam.mx>
2025-06-23 22:43:40 -0600

    tareita

--- interactividad1/sketch.js
+++ interactividad1/sketch.js
@@ -1,11 +1,22 @@
 let imgQ, imgW, imgE, imgR, imgT, imgY, imgU, imgI, imgO, imgP;
 let imagenActual = null;
+let fondo;
 
 
 function preload(){
+
+  fondo = loadImage('assets/fondo2.jpeg');
+
   imgQ = loadImage('assets/dancingqueen.jpg');
-  imgW = loadImage('assets/killerqueen.jpg');
-  imgE = loadImage('assets/easier.png')
+  imgW = loadImage('assets/won.jpg');
+  imgE = loadImage('assets/easier.png');
+  imgR = loadImage('assets/rockthatbody.jpg');
+  imgT = loadImage('assets/tearsdryon.jpeg');
+  imgY = loadImage('assets/youve.jpeg');
+  imgU = loadImage('assets/ucant.jpeg')
+  imgI = loadImage('assets/iwishyoulove.jpeg')
+  imgO = loadImage('assets/onandon.jpeg')
+  imgP = loadImage('assets/ponthereplay.jpeg')
 
 }
 function setup() {
@@ -20,7 +31,9 @@ function setup() {
 // }
 
 function draw (){
-  background(45, 7, 5);
+
+  image(fondo, 0, 0, width, height);
+
   if (imagenActual !==null){
     let imgRatio = imagenActual.width / imagenActual.height;
     let canvasRatio = width / height;
@@ -28,26 +41,57 @@ function draw (){
     let newWidth, newHeight;
 
     if (imgRatio > canvasRatio) {
-      newWidth = width * 0.8;
+      newWidth = width * 0.;6
       newHeight = newWidth / imgRatio;
     } else {
-      newHeight = height *0.8;
+      newHeight = height *0.6;
       newWidth = newHeight * imgRatio;
     }
 
     let x = (width - newWidth) / 2;
-    let y = (height - newHeight) / 2;
+    let y = (height - newHeight) / 4;
 
     image (imagenActual, x, y, newWidth, newHeight);
+
+    fill(190, 190, 190); 
+    //text(textoActual, width / 3, y + newHeight + 80); 
+    textAlign(CENTER, CENTER); // Centrado horizontal y vertical
+    text(textoActual, width / 2, height * 0.85);
+    textSize(30);
+    
   }
 }
 
 
 function keyPressed(){
-  if ( key === 'q'){ imagenActual=imgQ; }
-  
-  if ( key === 'w'){ imagenActual=imgW; }
+  if ( key === 'q'){ imagenActual=imgQ; 
+  textoActual = "Dancing Queen - ABBA";}
 
-  if ( key === 'e'){ imagenActual=imgE; }
+  else if ( key === 'w'){ imagenActual=imgW; 
+  textoActual = "Wonderwall - Oasis";}
+
+  else if ( key === 'e'){ imagenActual=imgE; 
+  textoActual = "Easier said than done - Thee Sacred Souls";}
+
+  else if ( key === 'r'){ imagenActual=imgR;
+  textoActual = "Rock that body - Black Eyed Peas";}
+
+  else if ( key === 't'){ imagenActual=imgT;
+  textoActual = "Tears dry on their own - Amy Winehouse";}
+
+  else if ( key === 'y'){ imagenActual=imgY;
+    textoActual = "You’ve Really Got a Hold on Me -  The Miracles";}
+
+  else if ( key === 'u'){ imagenActual=imgU;
+  textoActual = "U can't touch this - M.C. Hammer";}
+
+  else if ( key === 'i'){ imagenActual=imgI;
+  textoActual = "I wish you love - Laufey";}
+
+  else if ( key === 'o'){ imagenActual=imgO;
+  textoActual = "On & On - Erykah Badu";}
+
+  else if ( key === 'p'){ imagenActual=imgP;
+  textoActual = "Pon the replay - Rihanna";}
 
 }
